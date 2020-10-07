e4b578fbf44013c1829849b9f4eeb9f930d19ce5
BNivia <nobenjamin19@gmail.com>
2020-10-06 20:56:25 -0500

    Completed Exercise 2.

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,12 +1,7 @@
-"use strict";
-var state = [];
+//Naomi Benjamin
+//tic-tac-toe
 
-document.addEventListener('DOMContentLoaded', function () {
-	setgrid();
-	console.log('Content has been loaded...');
-}, false);
-
-
+//Function to display grid
 var setgrid = function(){
     var board = document.getElementById("board");
     var divs = board.querySelectorAll("div");
@@ -15,7 +10,50 @@ var setgrid = function(){
         }
 }
 
-var mark = function(){
+//array that holds all winning combinations
+let wins = [];
+
+document.addEventListener('DOMContentLoaded', function () {
+    var status = document.querySelector('#status');
+    var button = document.querySelector('.btn');
+    var state = [];
+    let o = true;
+
+    setgrid();
+
+    let sq = document.querySelectorAll('.square');
     
-}
+    sq.forEach(square => {
+        square.addEventListener('click', () =>{
+            if (o && square.textContent == ''){
+                square.textContent = 'O';
+                square.classList.add("O");
+                o = false;
+                state.push(square.textContent);
+            }
+
+            else if(!o && square.textContent == ''){
+                square.textContent = 'X';
+                square.classList.add("X");
+                o = true;
+                state.push(square.textContent);
+            }
+
+
+            
+            console.log(state);
+
+            
+
+            
+
+        });
+    });
+
+	//console.log('Content has been loaded...');
+}, false);
+
+
+
+
 
